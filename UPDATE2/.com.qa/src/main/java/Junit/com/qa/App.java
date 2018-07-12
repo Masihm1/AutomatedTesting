777ea8c36dd17a6c86b0b5a8261e2b22313eae2b
Jevale Sterling <jevale.sterling@qa.com>
2018-07-12 17:36:48 +0100

    Update2

---
+++ UPDATE2/.com.qa/src/main/java/Junit/com/qa/App.java
@@ -0,0 +1,13 @@
+package Junit.com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UPDATE2/StackExample/src/test/java/com/qa/StackExample/stackTest.java
@@ -0,0 +1,13 @@
+package com.qa.StackExample;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:")
+
+public class stackTest {
+
+}
